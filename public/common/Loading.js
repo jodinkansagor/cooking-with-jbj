cd5f067c9323bf1ca2df860ddcf9917a540a2c46
Jodi N Kansagor <joedvice@gmail.com>
2019-12-13 23:00:16 -0800

    got my fetch call working and loading spinner spinning

--- public/common/Loading.js
+++ public/common/Loading.js
@@ -2,13 +2,17 @@ import Component from '../Component.js';
 
 class Loading extends Component {
   renderHTML() {
+    const loading = this.props.loading;
+    if (!loading) {
+      return /*html*/ `
+          <div></div>`;
+    }
 
-
-    return /*html*/`
-            <div class="loading-container">
-                <img src="./assets/flyingfood.gif">
-            </div>
-        `;
+    return /*html*/ `
+        <div class = "loading-container">
+            <img src = "assets/flyingfood.gif">
+        </div>
+    `;
   }
 }
 
