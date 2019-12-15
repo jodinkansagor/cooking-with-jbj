767524494371e21ee4d170ea70db8edf4ac3d48a
Jodi N Kansagor <joedvice@gmail.com>
2019-12-15 08:26:36 -0800

    mobile first styling of main page done for now. want to add search and filter component

--- recipe-list/RecipeItem.js
+++ recipe-list/RecipeItem.js
@@ -7,12 +7,12 @@ class RecipeItem extends Component {
     return /*html*/ `
       <li class = "recipe-item">
         <a href="recipe-detail.html?id=${recipe._id}">
+        <div class = "image-container">
+            <img src="${recipe.imageURL}" alt = "${recipe.name}">
+          </div>
           <div class = "info-container">
             <h2>${recipe.name}</h2>
             <h3>${recipe.type}</h3>
-          </div>
-          <div class = "image-container">
-            <img src="${recipe.imageURL}" alt = "This is a bear">
           </div>
         </a>
       </li>
