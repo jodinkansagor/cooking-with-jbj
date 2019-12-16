d5a9e504ead888f8ffcb60a4c4ea6d25111807f9
Jodi N Kansagor <joedvice@gmail.com>
2019-12-15 19:19:34 -0800

    a bunch of styling and refactoring

--- recipe-detail/RecipeDetail.js
+++ recipe-detail/RecipeDetail.js
@@ -3,21 +3,38 @@ import Component from '../Component.js';
 class RecipeDetail extends Component {
 
   renderHTML() {
+
     const recipe = this.props.recipe;
-    console.log(recipe.attempts);
+    let recipeDirections = recipe.directions.join('\n');
+    let ingredientLine = recipe.ingredients.map(ingredient => {
+      return `${ingredient.name}: ${ingredient.amount} ${ingredient.measurements}`;
+    });
+    let attemptLine = recipe.attempts.map(attempt => {
+      return `${attempt.month}-${attempt.day}-${attempt.year}: ${attempt.notes} Rating: ${attempt.rating}`;
+    });
+
+    console.log(ingredientLine);
+    console.log(recipe.directions);
+    console.log(attemptLine);
+
+
     return /*html*/ `
       <div class = "recipe-detail">
         <h1>${recipe.name}</h2>
-          
-          <img src = "${recipe.imageURL}" />
-          <h3>Ingredients: ${recipe.ingredients.map(ingredient => {
-    return `${ingredient.name}: ${ingredient.amount} ${ingredient.measurements}`;
-  })}</h3>
-
-    <h3>Attempts: ${recipe.attempts.map(attempt => {
-    return `${attempt.month}-${attempt.day}-${attempt.year}: ${attempt.notes} Rating: ${attempt.rating}`;
-  })}</h3>
-    <h3>${recipe.type}</h3>
+        <img src = "${recipe.imageURL}" />
+        <section class = "ingredients">
+          <h4>Ingredients:</h4>
+          <p>${ingredientLine}</p>
+        </section>
+        <section class = "directions">
+          <h4>Directions:</h4>
+          <p>${recipeDirections}</p>
+        </section>
+        <section class = "attempts">
+          <h4>Attempts:</h4>
+          <p>${attemptLine}</p>
+        </section>
+        <h5>${recipe.type}</h5>
     `;
   }
 }
